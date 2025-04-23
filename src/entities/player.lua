2ef5241b155c3c0089e59df7940d01ec886f1273
Willem Helmet Pickleman <willemhelmet@gmail.com>
2025-04-23 11:59:38 -0400

    removed teleport component

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -4,7 +4,6 @@
 -- no it is not
 local class = require 'lib.30log'
 local TransformComponent = require 'src.components.transform-component'
-local TeleportComponent = require 'src.components.teleport-component'
 local PlayerLocomotionComponent = require 'src.components.player-locomotion'
 
 local Player = class('Player')
@@ -15,7 +14,6 @@ function Player:init(options)
   self.Pose = options.Pose or Mat4()
   self.Transform = options.Transform or TransformComponent()
   self.Velocity = options.Velocity or Vec3(0, 0, 0)
-  self.Teleport = options.Teleport or TeleportComponent()
   self.PlayerLocomotion = options.PlayerLocomotion or PlayerLocomotionComponent()
 end
 
